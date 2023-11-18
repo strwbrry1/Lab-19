03df59545f2294280f343c3bdfc967cc71b87471
strwbrry1 <matvienkosemyon@gmail.com>
2023-11-18 21:43:44 +0400

    Добавьте файлы проекта.

---
+++ Lab 19/Lab 19.c
@@ -0,0 +1,67 @@
+﻿#define _CRT_SECURE_NO_WARNINGS
+
+#include "stdio.h"
+
+void load();
+void saveRes();
+
+int n;
+int* pa;
+
+int main()
+{
+	SetConsoleCP(1251);
+	SetConsoleOutputCP(1251);
+
+	load();
+	saveRes();
+
+	free(pa);
+}
+
+void load() {
+	FILE* fin = fopen("in2.txt", "rt");
+	if (fin == NULL) {
+		printf("Входной файл не найден");
+		return;
+	}
+
+	fscanf(fin, "%d", &n);
+
+	pa = (int*)malloc(sizeof(int) * n);
+
+	for (int i = 0; i < n; i++) {
+		fscanf(fin, "%d", &pa[i]);
+	}
+	fclose(fin);
+}
+
+void saveRes() {
+	float res;
+	int sum = 0;
+	for (int i = 0; i < n; i++) {
+		sum += pa[i];
+	}
+	res = sum / n;
+	
+	int m = 0;
+	for (int i = 0; i < n; i++) {
+		if (pa[i] > res) {
+			m++;
+		}
+	}
+
+	FILE* fout = fopen("out2.txt", "wt");
+	if (fout == NULL) {
+		printf("Выходной файл не найден");
+		return;
+	}
+	fprintf(fout, "%d\n", m);
+	for (int i = 0; i < n; i++) {
+		if (pa[i] > res) {
+			fprintf(fout, "%d ", pa[i]);
+		}
+	}
+	fclose(fout);
+}
+
---
+++ n1.c
@@ -0,0 +1,63 @@
+﻿#define _CRT_SECURE_NO_WARNINGS
+
+#include "stdio.h"
+
+void load();
+void saveRes();
+
+int n;
+int avg = 0;
+int a[1000];
+
+int main()
+{
+	SetConsoleCP(1251);
+	SetConsoleOutputCP(1251);
+
+	load();
+	saveRes();
+
+}
+
+void load() {
+	FILE* fin = fopen("in1.txt", "rt");
+	if (fin == NULL) {
+		printf("Входной файл не найден");
+		return;
+	}
+
+	fscanf(fin, "%d", &n);
+	for (int i = 0; i < n; i++) {
+		fscanf(fin, "%d", &a[i]);
+	}
+	fclose(fin);
+}
+
+void saveRes() {
+	float res;
+	int sum = 0;
+	for (int i = 0; i < n; i++) {
+		sum += a[i];
+	}
+	res = sum / n;
+	
+	int m = 0;
+	for (int i = 0; i < n; i++) {
+		if (a[i] > res) {
+			m++;
+		}
+	}
+
+	FILE* fout = fopen("out1.txt", "wt");
+	if (fout == NULL) {
+		printf("Выходной файл не найден");
+		return;
+	}
+	fprintf(fout, "%d\n", m);
+	for (int i = 0; i < n; i++) {
+		if (a[i] > res) {
+			fprintf(fout, "%d ", a[i]);
+		}
+	}
+}
+
